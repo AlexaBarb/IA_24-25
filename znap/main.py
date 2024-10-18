81daac62d446e41b09dda0098b5e002be99a18b3
leo152004 <leandrogoncalves2004@gmail.com>
2024-10-18 15:59:41 +0100

    primeiro teste

--- znap/main.py
+++ znap/main.py
@@ -24,6 +24,7 @@ middle_motor= Motor(Port.B)
 right_motor = Motor(Port.C)
 left_motor = Motor(Port.A)
 
+'''
 middle_motor.run(1000)
 wait(2000)
 middle_motor.stop()
@@ -71,3 +72,157 @@ left_motor.run_angle(1000,900)
 middle_motor.run(-1000)
 wait(2000)
 middle_motor.stop()
+'''
+
+# Lista que armazenará os símbolos associados às cores detetadas (por exemplo, "+", "*", etc.).
+pecas = []
+ev3 = EV3Brick() 
+
+# Sensor de cor
+sensorCor = ColorSensor(Port.S1) 
+
+# Deteta a cor passada no sensor de cores
+def detetaCor():
+    # Lê o valor da cor
+    corDetetada = sensorCor.color()
+
+    # Mapa de valores de cor para nomes de cor
+    listaCores = {
+        Color.BLUE: 'Azul',
+        Color.GREEN: 'Verde',
+        Color.YELLOW: 'Amarelo',
+        Color.RED: 'Vermelho',
+    }
+
+    # Determina a cor detectada
+    nomeCorDetetada = listaCores.get(corDetetada, 'Desconhecida')
+    # Retorna o nome da cor detectada
+    return nomeCorDetetada
+
+# Deteta a cor das peças passadas e adiciona no array das peças
+def detetaCorPecas():
+    # Lê o nome da cor
+    corDetetada = detetaCor()
+    # Imprime a cor lida
+    print('Cor detectada: ' + corDetetada)
+
+    # Adiciona a peça no array dependendo da cor
+    if corDetetada == 'Verde':
+        pecas.append("+")
+        ev3.speaker.say("Green")
+        
+    elif corDetetada == 'Vermelho':
+        pecas.append("*")
+        ev3.speaker.say("Red")
+        
+    elif corDetetada == 'Amarelo':
+        pecas.append("0")
+        ev3.speaker.say("Yellow")
+        
+    elif corDetetada == 'Azul':
+        pecas.append("-")
+        ev3.speaker.say("Blue")
+
+# Período da deteção de peças            
+def leituraObjetos():
+    # Enquanto está no período de deteção
+    while True:
+        # Deteta e guarda as peças
+        detetaCorPecas()
+        # Espera 2 segundos
+        wait(2000)
+
+localizacao = [1,1]
+direcao = "frente"
+
+Cor_Sensor = ColorSensor(Port.S1)
+
+#cor barreiras -> vermelho
+#cor linhas -> preto
+#cor limitação de ambiente -> verde
+while True:
+    print(Cor_Sensor.color())
+    #Se o sensor de cor detectar a cor preta, o robô andará para frente
+    if datetaCor() == Cor_Sensor.COLOR_BLACK:
+        speed = 500
+        left_motor.run(speed)
+        right_motor.run(speed)
+        wait(5000)
+        left_motor.stop()
+        right_motor.stop()
+    #Se o sensor de cor detectar outra cor, da uma volta 
+    elif datetaCor() == None :
+        left_motor.run_angle(1000,-900)
+        left_motor.stop()
+        right_motor.stop()
+    #Se o sensor de cor detectar a cor vermelha/barreira, o robô dará uma volta
+    elif datetaCor() == Cor_Sensor.COLOR_RED :
+        left_motor.run_angle(1000,900)
+        left_motor.stop()
+        right_motor.stop()
+    elif datetaCor() == Cor_Sensor.COLOR_BROWN:
+        left_motor.run_angle(1000,900)
+        left_motor.stop()
+        right_motor.stop()
+
+# movimento
+
+speed = 500
+
+def andar():
+    left_motor.run(speed)
+    right_motor.run(speed)
+    wait(1850)
+    left_motor.stop()
+    right_motor.stop()
+    if direcao == "frente":
+        localizacao[1] += 1
+    if direcao == "direita":
+        localizacao[0] += 1
+    if direcao == "atras":
+        localizacao[1] -= 1
+    if direcao == "esquerda":
+        localizacao[0] -= 1
+
+def rodar_direita():
+    left_motor.run(speed)
+    right_motor.run(speed)
+    wait(700)
+    left_motor.stop()
+    right_motor.stop()
+    left_motor.run_angle(1000, -680)
+    if direcao == "frente":
+        direcao = "direita"
+    if direcao == "direita":
+        direcao = "atras"
+    if direcao == "atras":
+        direcao = "esquerda"
+    if direcao == "esquerda":
+        direcao = "frente"
+
+def rodar_esquerda():
+    left_motor.run(speed)
+    right_motor.run(speed)
+    wait(700)
+    left_motor.stop()
+    right_motor.stop()
+    right_motor.run_angle(1000, -680)
+    if direcao == "frente":
+        direcao = "esquerda"
+    if direcao == "esquerda":
+        direcao = "atras"
+    if direcao == "atras":
+        direcao = "direita"
+    if direcao == "direita":
+        direcao = "frente"
+
+leituraObjetos()
+print(detetaCorPecas())
+
+andar()
+wait(2000)
+rodar_direita()
+wait(1000)
+andar()
+wait(1000)
+rodar_esquerda()
