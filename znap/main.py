982c576ea783287f56ce8fc7b33bcdd38dae9f33
Alexandra Barbeito <alexandrabarbeito@outlook.pt>
2024-10-25 14:19:56 +0100

    Fit/alexa resolvendo conflito
    Co-authored-by: francisco3222 <francisco3222@users.noreply.github.com>

--- znap/main.py
+++ znap/main.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 
-"""
-Example LEGO® MINDSTORMS® EV3 Znap Program
-------------------------------------------
-
-This program requires LEGO® EV3 MicroPython v2.0.
-Download: https://education.lego.com/en-us/support/mindstorms-ev3/python-for-ev3
-
-Building instructions can be found at:
-https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#building-expansion
-"""
+#imports das bibliotecas necessárias
 from pybricks import ev3brick as brick
 from pybricks.hubs import EV3Brick 
 from pybricks.ev3devices import Motor, InfraredSensor,ColorSensor
@@ -19,10 +10,14 @@ from pybricks.tools import wait, StopWatch
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from random import randint
 
+#criação de uma instancia do módulo principal 
 ev3= EV3Brick()
+
+#inicialização dos motores
 middle_motor= Motor(Port.B)
 right_motor = Motor(Port.C)
 left_motor = Motor(Port.D)
+
 colorSensor = ColorSensor(Port.S2)
 
 def color():
@@ -43,88 +38,7 @@ def color():
             middle_motor.stop()
         contador += 1
     
-
-
-    
-
-
 '''
-
-
-middle_motor.run(1000)
-wait(2000)
-middle_motor.stop()
-
-i = 0
-while i < 2:
-    ev3.speaker.beep(200, 100)  # C
-    wait(100)
-    ev3.speaker.beep(200, 100)  # C
-    wait(100)
-    ev3.speaker.beep(200, 100)  # C
-    wait(100)
-    ev3.speaker.beep(300, 250)  # D
-    wait(200)
-    ev3.speaker.beep(400, 100)  # E
-    wait(250)
-    i = i + 1
-
-# Parte final da música
-wait(100)
-ev3.speaker.beep(400, 200)  # F
-wait(200)
-ev3.speaker.beep(400, 100)  # F
-wait(100)
-ev3.speaker.beep(350, 100)  # D
-wait(100)
-ev3.speaker.beep(350, 100)  # D
-wait(100)
-ev3.speaker.beep(300, 100)  # D
-wait(100)
-ev3.speaker.beep(300, 100)  # D
-wait(100)
-ev3.speaker.beep(200, 150)  # G
-#FAZER MOVIMENTO
-left_motor.run_angle(1000,-900)
-speed = 500
-left_motor.run(speed)
-right_motor.run(speed)
-wait(5000)
-left_motor.stop()
-right_motor.stop()
-left_motor.run_angle(1000,900)
-
-#MOVER A GARRA
-middle_motor.run(-1000)
-wait(2000)
-middle_motor.stop()
-
-
-# Lista que armazenará os símbolos associados às cores detetadas (por exemplo, "+", "*", etc.).
-pecas = []
-ev3 = EV3Brick() 
-
-# Sensor de cor
-sensorCor = ColorSensor(Port.S1) 
-
-# Deteta a cor passada no sensor de cores
-def detetaCor():
-    # Lê o valor da cor
-    corDetetada = sensorCor.color()
-
-    # Mapa de valores de cor para nomes de cor
-    listaCores = {
-        Color.BLUE: 'Azul',
-        Color.GREEN: 'Verde',
-        Color.YELLOW: 'Amarelo',
-        Color.RED: 'Vermelho',
-    }
-
-    # Determina a cor detectada
-    nomeCorDetetada = listaCores.get(corDetetada, 'Desconhecida')
-    # Retorna o nome da cor detectada
-    return nomeCorDetetada
-
 # Deteta a cor das peças passadas e adiciona no array das peças
 def detetaCorPecas():
     # Lê o nome da cor
@@ -160,16 +74,15 @@ def leituraObjetos():
 
 localizacao = [1,1]
 direcao = "frente"
-
-Cor_Sensor = ColorSensor(Port.S1)
-
+'''
 #cor barreiras -> vermelho
 #cor linhas -> preto
-#cor limitação de ambiente -> verde
+#cor manteiga -> amarelo
+
 while True:
-    print(Cor_Sensor.color())
+    print(cor=colorSensor.color())
     #Se o sensor de cor detectar a cor preta, o robô andará para frente
-    if datetaCor() == Cor_Sensor.COLOR_BLACK:
+    if colorSensor.color() == colorSensor.COLOR_BLACK:
         speed = 500
         left_motor.run(speed)
         right_motor.run(speed)
@@ -177,24 +90,25 @@ while True:
         left_motor.stop()
         right_motor.stop()
     #Se o sensor de cor detectar outra cor, da uma volta 
-    elif datetaCor() == None :
+    elif colorSensor.color() == None :
         left_motor.run_angle(1000,-900)
         left_motor.stop()
         right_motor.stop()
     #Se o sensor de cor detectar a cor vermelha/barreira, o robô dará uma volta
-    elif datetaCor() == Cor_Sensor.COLOR_RED :
+    elif colorSensor.color() == colorSensor.COLOR_RED :
         left_motor.run_angle(1000,900)
         left_motor.stop()
         right_motor.stop()
-    elif datetaCor() == Cor_Sensor.COLOR_BROWN:
+    elif colorSensor.color() == colorSensor.COLOR_BROWN:
         left_motor.run_angle(1000,900)
         left_motor.stop()
         right_motor.stop()
 
 # movimento
-'''
+
 speed = 250
 direcao = ""
+
 def andar():
     left_motor.run(speed/2)
     right_motor.run(speed/2)
@@ -261,9 +175,3 @@ def rodar_esquerda():
         direcao = "direita"
     if direcao == "direita":
         direcao = "frente"
-
-
-andar()
-andar()
-color()
-rodar_direita()
